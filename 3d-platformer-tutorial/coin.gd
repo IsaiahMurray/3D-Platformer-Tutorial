4f03539d4991188a3f7d02a90a917b609599ed17
Isaiah Murray <murray.isaiah13@gmail.com>
2025-01-29 16:00:07 -0500

    Expand coin functionality
    -Added function to delete on collision

--- 3d-platformer-tutorial/coin.gd
+++ 3d-platformer-tutorial/coin.gd
@@ -5,6 +5,10 @@ const ROTATION_SPEED = 2 # number of degrees the coin will spin every frame
 func rotate_coin() -> void:
 	rotate_y(deg_to_rad(ROTATION_SPEED))
 
+func delete_coin() -> void: 
+	if has_overlapping_bodies():
+		queue_free() 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,3 +17,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate_coin()
+	delete_coin()
