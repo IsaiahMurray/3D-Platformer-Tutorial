fbb92e396447a13531a96ecdccd4a821de3a0add
Isaiah Murray <murray.isaiah13@gmail.com>
2025-01-29 16:20:15 -0500

    Updated collision handling
    -changed if statement to a signal to handle collision detection

--- 3d-platformer-tutorial/coin.gd
+++ 3d-platformer-tutorial/coin.gd
@@ -5,10 +5,6 @@ const ROTATION_SPEED = 2 # number of degrees the coin will spin every frame
 func rotate_coin() -> void:
 	rotate_y(deg_to_rad(ROTATION_SPEED))
 
-func delete_coin() -> void: 
-	if has_overlapping_bodies():
-		queue_free() 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,4 +13,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate_coin()
-	delete_coin()
+
+
+
+func _on_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
