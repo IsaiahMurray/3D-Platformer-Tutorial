044e4e1d87e38bd7cd81fffb8f6aea6ba19c4b25
Isaiah Murray <murray.isaiah13@gmail.com>
2025-01-31 12:24:18 -0500

    Updated player script
    -Added controls to orbit camera left and right

--- 3d-platformer-tutorial/player.gd
+++ 3d-platformer-tutorial/player.gd
@@ -4,8 +4,15 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 12
 
-
 func _physics_process(delta: float) -> void:
+	
+	# Orbit camera around player
+	if Input.is_action_just_pressed("camera_left"):
+		$Camera_Controller.rotate_y(deg_to_rad(30))
+	
+	if Input.is_action_just_pressed("camera_right"):
+		$Camera_Controller.rotate_y(deg_to_rad(-30))
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -17,7 +24,8 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	var direction = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
