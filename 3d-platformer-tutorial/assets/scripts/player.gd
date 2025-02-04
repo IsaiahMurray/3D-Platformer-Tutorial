0308415c804b89afc810fde258527b41b2d435a4
Isaiah Murray <murray.isaiah13@gmail.com>
2025-02-04 13:54:55 -0500

    Importing 3D character model
    -Imported model
    -Assigned model to player object
    -Adjusted code to animate new character model

--- 3d-platformer-tutorial/assets/scripts/player.gd
+++ 3d-platformer-tutorial/assets/scripts/player.gd
@@ -7,6 +7,16 @@ var xForm : Transform3D
 
 func _physics_process(delta: float) -> void:
 	
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	# Play character animations
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		$AnimationPlayer.play("jump")
+	elif is_on_floor() and input_dir != Vector2.ZERO:
+		$AnimationPlayer.play("run")
+	elif is_on_floor() and input_dir == Vector2.ZERO:
+		$AnimationPlayer.play("idle")
 	# Orbit camera around player
 	if Input.is_action_just_pressed("camera_left"):
 		$Camera_Controller.rotate_y(deg_to_rad(30))
@@ -22,15 +32,13 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	# New Vector 3 to account for user arrow inputs and camera rotation
 	var direction = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	# Rotate character mesh to face the direction the player is moving
 	if input_dir != Vector2(0, 0):
-		$MeshInstance3D.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) - 90
+		$Armature.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) - 90
 	
 	# Rotatae the character to align with the floor
 	if is_on_floor():
