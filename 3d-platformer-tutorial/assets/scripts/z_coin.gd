b38bffd2ee25c431048e1d7d96cc1005f16877b9
Isaiah Murray <murray.isaiah13@gmail.com>
2025-02-04 13:53:43 -0500

    Finishing HUD and coin collection

--- 3d-platformer-tutorial/assets/scripts/z_coin.gd
+++ 3d-platformer-tutorial/assets/scripts/z_coin.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
 const ROTATION_SPEED = 2 # number of degrees the coin will spin every frame
+@export var hud : CanvasLayer
 
 func rotate_coin() -> void:
 	rotate_y(deg_to_rad(ROTATION_SPEED)) # Rotate coin
@@ -15,6 +16,7 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node3D) -> void:
 	Global.coins += 1
+	hud.get_node("CoinsLabel").text = str(Global.coins)
 	print("You have ", Global.coins, " coins")
 	set_collision_layer_value(3, false) # Turn layer 3 off
 	set_collision_mask_value(1, false) # Turn mask 1 off
